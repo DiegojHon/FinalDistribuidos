bfa616e653045b5b0e941efda7d6d4f9ae0b4fc5
dataprueba94@gmail.com <dataprueba94@gmail.com>
2023-07-26 17:56:31 -0500

    eureka-service

---
+++ config-servicess/src/test/java/com/configservicess/ConfigServicessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configservicess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServicessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
